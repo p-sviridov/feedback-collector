14b05a121dbba1657222474250a16facec621edd
Pavel Sviridov <pavlentiysv@gmail.com>
2021-08-23 11:27:21 -0400

    restore prev auth configuration

--- services/passport.js
+++ services/passport.js
@@ -1,11 +1,12 @@
 const passport = require('passport');
+const keys = require('../config/keys');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 passport.use(
   new GoogleStrategy(
     {
-      clientID: process.env.googleClientID,
-      clientSecret: process.env.googleClientSecret,
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback',
     },
     (accessToken, refreshToken, profile, done) => {

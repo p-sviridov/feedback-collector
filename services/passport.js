3a33a09b60cd7b8fcb503d19d6ec38ecd531d1a5
Pavel Sviridov <pavlentiysv@gmail.com>
2021-08-23 10:59:02 -0400

    fix for heroku

--- services/passport.js
+++ services/passport.js
@@ -5,8 +5,8 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 passport.use(
   new GoogleStrategy(
     {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
+      clientID: keys.googleClientID || process.env.googleClientID,
+      clientSecret: keys.googleClientSecret || process.env.googleClientSecret,
       callbackURL: '/auth/google/callback',
     },
     (accessToken, refreshToken, profile, done) => {

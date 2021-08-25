9ce3e20cbf04a7602cae3e491de2160294326e31
Pavel Sviridov <pavlentiysv@gmail.com>
2021-08-25 16:06:22 -0400

    test of prod env

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,7 @@
 if (process.env.NODE_ENV === 'produciton') {
+  console.log('in true', process.env.NODE_ENV);
   module.exports = require('./prod');
 } else {
+  console.log('in false', process.env.NODE_ENV);
   module.exports = require('./dev');
 }
